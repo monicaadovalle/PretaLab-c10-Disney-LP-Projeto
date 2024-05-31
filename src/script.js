df651b02c4bcb8b3541a719d3966d01e3792177c
Monica do Valle <monicadovalle@outlook.com>
2024-05-31 18:46:02 -0300

    adicionando pontuação

--- src/script.js
+++ src/script.js
@@ -12,10 +12,16 @@ function jogoDeAdivinhacao() {
     if (PalpiteDigitado === numeroAleatorio) {
         alert("Parabéns, você ganhou")
     } else if (PalpiteDigitado > numeroAleatorio) {
+        tentativas++;
         atualizarFeedback("Esse número é muito maior, tente novamente")
     } else {
+        tentativas++;
         atualizarFeedback("O número é muito baixo, tente de novo")
     }
+    const novaPontuacao = 100 - (tentativas*10);
+    atualizarPontuacao(novaPontuacao)
+    console.log (tentativas)
+
 }
 
 function reiniciarJogo() {
